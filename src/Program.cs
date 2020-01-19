46198abc75eb34245e8aec7dcbf8f333888ebac9
Szymon <szymeklaciak@gmail.com>
2020-01-19 14:20:09 +0000

    Create src directory
    TODO

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace Saper
+﻿namespace Saper
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
